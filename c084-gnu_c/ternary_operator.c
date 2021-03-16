5aba0ac5239ec2bb7e3de1b2d146bc74dd763dbc
snoire <snoire@qq.com>
2021-03-16 18:32:35 +0800

    add c084-gnu_c/

--- c084-gnu_c/ternary_operator.c
+++ c084-gnu_c/ternary_operator.c
@@ -1,4 +1,9 @@
 #include <stdio.h>
+/* 这好像是 gnu 的扩展语法 */
+/* https://gcc.gnu.org/onlinedocs/gcc-10.2.0/gcc/Conditionals.html#Conditionals */
+
+/* the expression `x ? : y` has the value of x if that is nonzero; otherwise, the value of y.
+   This example is perfectly equivalent to `x ? x : y` */
 
 int main()
 {
