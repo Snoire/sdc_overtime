78905e17d8fe682f3d7cb64509d0252eaa9c862f
snoire <snoire@qq.com>
2021-05-13 16:45:54 +0800

    update

--- c086-mmap/map_normalfile1.c
+++ c086-mmap/map_normalfile1.c
@@ -1,4 +1,5 @@
 // https://www.cnblogs.com/codestack/p/11188889.html
+/* 先执行 map_normalfile2，再执行 map_normalfile1 */
 #include <stdio.h>
 #include <string.h>
 #include <sys/mman.h>
@@ -15,6 +16,7 @@ typedef struct {
 int main(int argc, char **argv)     // map a normal file as shared mem:
 {
     int fd, i;
+    struct stat st;
     people *p_map;
     char temp;
 
@@ -24,10 +26,21 @@ int main(int argc, char **argv)     // map a normal file as shared mem:
     }
 
     fd = open(argv[1], O_CREAT | O_RDWR | O_TRUNC, 00777);
+    stat(argv[1], &st);
+    printf("st.size: %ld\n", st.st_size);
+
+#if 0
     lseek(fd, sizeof(people) * 5 - 1, SEEK_SET);
     write(fd, "", 1);
+#endif
+    printf("sizeof(people): %lu\n", sizeof(people));
+    truncate(argv[1], sizeof(people)*5);
+
+    stat(argv[1], &st);
+    printf("st.size: %ld\n", st.st_size);
 
     p_map = (people *) mmap(NULL, sizeof(people) * 10, PROT_READ | PROT_WRITE, MAP_SHARED, fd, 0);
+    printf("mmap succeeded\n");
     close(fd);
     temp = 'a';
 
@@ -38,11 +51,15 @@ int main(int argc, char **argv)     // map a normal file as shared mem:
     }
 
     printf("initialize over\n");
-    sleep(15);
+    sleep(15);   /* 注释这一行 */
 
     /* 写回磁盘文件的内容不能超过文件的大小，所以可能需要用 truncate 调整文件大小 */
+    /* 只要没执行到这，map2 可以访问超过文件大小 */
     munmap(p_map, sizeof(people) * 10);
     printf("umap ok\n");
+    stat(argv[1], &st);
+    printf("st.size: %ld\n", st.st_size);
+    /* 如果 map1 提前退出，map2 只能访问文件大小范围内的数据 */
 
     return 0;
 }
--- c086-mmap/map_normalfile2.c
+++ c086-mmap/map_normalfile2.c
@@ -1,6 +1,8 @@
+/* 先执行 map_normalfile2，再执行 map_normalfile1 */
 #include <stdio.h>
 #include <sys/mman.h>
 #include <sys/types.h>
+#include <sys/stat.h>
 #include <fcntl.h>
 #include <unistd.h>
 
@@ -12,6 +14,7 @@ typedef struct {
 int main(int argc, char **argv)     // map a normal file as shared mem:
 {
     int fd, i;
+    struct stat st;
     people *p_map;
 
     if (argc < 2) {
@@ -19,8 +22,17 @@ int main(int argc, char **argv)     // map a normal file as shared mem:
         return -1;
     }
 
-    fd = open(argv[1], O_CREAT | O_RDWR, 00777);
-    p_map = (people *) mmap(NULL, sizeof(people) * 10, PROT_READ | PROT_WRITE, MAP_SHARED, fd, 0);
+    fd = open(argv[1], O_CREAT | O_TRUNC | O_RDWR, 00777);
+    p_map = (people *) mmap(NULL, sizeof(people) * 5, PROT_READ | PROT_WRITE, MAP_SHARED, fd, 0);
+    printf("mmap succeeded\n");
+    close(fd);  /* 关闭不影响 mmap */
+    
+    stat(argv[1], &st);
+    printf("st.size: %ld\n", st.st_size);
+
+    sleep(10);
+    stat(argv[1], &st);
+    printf("st.size: %ld\n", st.st_size);
 
     for (i = 0; i < 10; i++) {
         printf("name: %s age %d;\n", (*(p_map + i)).name, (*(p_map + i)).age);
