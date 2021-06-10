d804874aff74c3c361fb8528fbaf4f89007a0e9b
snoire <snoire@qq.com>
2021-06-10 17:31:26 +0800

    update memset.c

--- c063-test/memset.c
+++ c063-test/memset.c
@@ -1,9 +1,11 @@
 #include <stdio.h>
+#include <string.h>
 
 int main()
 {
-    int n[2][4];
-    memset(n, 0xFF, sizeof(n));
+    char n[2][4];
+//    memset(n, 0xFF, sizeof(n));
+    memset(n, 0x7F, sizeof(n));     /* 最大正数(1 byte)，127 */
 
     for (int i = 0; i < 2; i++) {
         for (int j = 0; j < 4; j++) {
