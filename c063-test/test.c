290dc2b48c0b34048ff9c9c4e6b9de998b1ddb22
snoire <snoire@qq.com>
2021-06-11 11:44:20 +0800

    update test.c

--- c063-test/test.c
+++ c063-test/test.c
@@ -3,6 +3,20 @@
 
 int main()
 {
+#if 0
+    int sum = 0;
+    unsigned char noise = -81;
+    printf("(int) noise: %d\n", (int) noise);
+    printf("noise: %d\n", noise);
+    sum += noise;
+    printf("sum: %d\n", sum);
+#elif 1
+    signed char a = -81; /* char 有可能是 signed char，也有可能是 unsigned char */
+    unsigned char b = a;
+    printf("a: %hhu\n", a);
+    printf("b: %hhd\n", b);
+#else
+
     char *str = "0";
 //    char *str2 = "56";
 
@@ -14,6 +28,7 @@ int main()
     str = NULL;
     if (*str != 0)
         printf("str != 0");
+#endif
 
     return 0;
 }
